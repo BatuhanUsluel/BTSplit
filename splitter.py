cd2807c3712694d8d57e441ba987a0e76b0c52be
Batuhan Usluel <batuhan.usluel@gmail.com>
2019-07-26 00:25:21 +0300

    Working Prototype
    Splitter file reads the csv in chunks, writing them to new files as it goes.
    Also removed index column from test file.

--- splitter.py
+++ splitter.py
@@ -1,8 +1,28 @@
 import argparse
+import pandas as pd
 
 parser = argparse.ArgumentParser("CSV Splitter")
 parser.add_argument("-f", "--file", help="File Name(Required)", type=str, required=True)
 parser.add_argument("-r", "--rows", help="Custom Number of rows per file[defualt: 1048576]", type=int, default=1048576, required=False)
 parser.add_argument("-of", "--outputfile", help="Output file Prefix[defualt: input filename]", type=str, required=False)
-parser.add_argument("-t", "--transition", help="Output file transition[defualt: _]", type=str, required=False)
+parser.add_argument("-t", "--transition", help="Output file transition[defualt: _]", type=str, required=False, default = "_")
 args = parser.parse_args()
+print ("Output FIle:")
+print (args.outputfile)
+
+sep = '.'
+file_no_extension = args.file.split(sep, 1)[0]
+
+if (args.outputfile==None):
+    args.outputfile = file_no_extension
+    
+i=0
+chunksize = args.rows
+first=True
+for chunk in pd.read_csv(args.file, chunksize=chunksize):
+    chunk.to_csv(args.outputfile + args.transition + str(i) + ".csv", sep=',', index=False, header=first)
+    print("-----------------------------------------")
+    print(chunk)
+    i=i+1
+    if (first):
+        first=False
--- test_creator.py
+++ test_creator.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import numpy as np
 df = pd.DataFrame(columns=['Index'])
-df['Index'] = range(0, 10000)#1500000
-df.to_csv("random.csv", sep=',')
+df['Index'] = range(1, 10000)#1500000
+df.to_csv("random.csv", sep=',', index=False, header=False)
